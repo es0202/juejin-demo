4bb18c77505421d6ff1efd9211ac84abcce10e9b
es0202 <LiHuan0202es@163.com>
2019-10-11 22:28:51 +0800

    新增回滚顶部组件

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,24 @@
     <headers></headers>
     <router-view></router-view>
     <svg-icon></svg-icon>
+    <suspension v-show="isShow"></suspension>
   </div>
 </template>
 
 <script>
 import headers from './components/header/header';
 import svgIcon from './components/common/svg';
+import suspension from './components/common/suspension';
 export default {
   name: 'App',
+  data() {
+    return {
+      isShow: false
+    };
+  },
   mounted() {
     let beforeScroll = window.scrollY;
+    const that = this;
     window.addEventListener('scroll', function() {
       let afterScroll = window.scrollY;
       if (window.scrollY > 150 && afterScroll - beforeScroll > 150) {
@@ -27,11 +35,17 @@ export default {
         document.getElementsByClassName('tag-wrap')[0].className = 'tag-wrap';
         document.getElementsByClassName('sticky-block')[0].className = 'sticky-block';
       }
+      if (window.scrollY > document.documentElement.clientHeight) {
+        that.isShow = true;
+      } else {
+        that.isShow = false;
+      }
     });
   },
   components: {
     headers,
-    svgIcon
+    svgIcon,
+    suspension
   }
 };
 </script>
---
+++ src/components/common/suspension.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="suspension-panel">
+    <el-button icon="el-icon-caret-top" @click.native="scrollTop(0,500)"></el-button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    scrollTop(number = 0, time) {
+      //scrollTop不能用箭头函数,this会指向window
+      //平滑滚动至顶部
+      // if (!time) {
+      //   document.body.scrollTop = document.documentElement.scrollTop = number;
+      //   return number;
+      // }
+      // const spacingTime = 20; // 设置循环的间隔时间  值越小消耗性能越高
+      // let spacingInex = time / spacingTime; // 计算循环的次数
+      // let nowTop = document.body.scrollTop + document.documentElement.scrollTop; // 获取当前滚动条位置
+      // let everTop = (number - nowTop) / spacingInex; // 计算每次滑动的距离
+      // let scrollTimer = setInterval(() => {
+      //   if (spacingInex > 0) {
+      //     spacingInex--;
+      //     this.scrollTop((nowTop += everTop));
+      //   } else {
+      //     clearInterval(scrollTimer); // 清除计时器
+      //   }
+      // }, spacingTime);
+
+      //一次到位
+      document.documentElement.scrollTop = 0;
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.suspension-panel {
+  position: fixed;
+  right: 24px;
+  bottom: 24px;
+  /deep/ .el-button {
+    border-radius: 50%;
+    padding: 12px;
+  }
+}
+</style>
