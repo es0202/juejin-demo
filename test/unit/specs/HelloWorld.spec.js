10248f307cfc72c0da8be4d920b550c0acd37c5b
es0202 <LiHuan0202es@163.com>
2019-11-07 10:19:01 +0800

    新增单元测试文件

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,43 @@
+import {
+  shallowMount,
+  mount,
+  createLocalVue
+} from "@vue/test-utils"
+import ElementUI from 'element-ui'
 import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld'
+import NestedRoute from "@/test.vue"
+import header from '@/components/header/header'
+Vue.use(ElementUI)
+describe("header", () => {
+  const $route = {
+    path: '/home/',
+    matched:[{path: "/home"}]
+    // ...其他属性
+  }
 
-describe('HelloWorld.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .toEqual('Welcome to Your Vue.js App')
+  const $router = {
+    push: '/home/recommend'
+    // ... 其他属性
+  }
+
+  const wrapper = mount(header, {
+    mocks: {
+      $route,
+      $router
+    },stubs: ['router-link', 'router-view']
+  })
+  it('render test', () => {
+    // const wrapper = mount(header)
+    expect(wrapper.html()).toContain('<div class="test"></div>')
+  })
+
+
+  it("renders a child component via routing", () => {
+    // const wrapper = mount(App, { localVue, router })
+
+    // router.push("/nested-route")
+
+    // expect(wrapper.find(NestedRoute).exists()).toBe(true)
+
   })
 })
